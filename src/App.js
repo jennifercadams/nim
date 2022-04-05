1213ff23b3d9049631e373d6aab16c636715c884
Jennifer Adams <wordsword23@gmail.com>
2022-04-05 13:59:07 -0400

    added start, game, and difficulty to state and startGame method

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { useState } from 'react';
+
 import './App.css';
 import Start from './Components/Start/Start';
 import Game from './Components/Game/Game';
 
 function App() {
+  const [ start, setStart ] = useState(true);
+  const [ game, setGame ] = useState(false);
+  const [ difficulty, setDifficulty ] = useState('Impossible');
+
+  const startGame = () => {
+    setStart(false);
+    setGame(true);
+  }
+
   return (
     <div className="App">
       <h1>Let's Play Nim!</h1>
-      <Start />
-      <Game />
+      {start && <Start />}
+      {game && <Game />}
     </div>
   );
 }

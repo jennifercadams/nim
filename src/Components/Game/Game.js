9b3b097ffa6e5ef86f838a2407ecd4a6cfc985b6
Jennifer Adams <wordsword23@gmail.com>
2022-04-05 15:48:19 -0400

    implemented impossible game logic and win conditions

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -4,6 +4,7 @@ import './Game.css';
 
 function Game({ difficulty }) {
   const [ coinsLeft, setCoinsLeft ] = useState(12);
+  const [ winner, setWinner ] = useState(null);
   const [ lastPlayerMove, setLastPlayerMove ] = useState(null);
   const [ lastNimMove, setLastNimMove ] = useState(null);
 
@@ -12,24 +13,47 @@ function Game({ difficulty }) {
     coins.push(<div className="coin" key={i}>$</div>)
   }
 
-  const handleTurn = (e) => {
-    const choice = e.target.value;
+  const impossibleNim = (choice) => {
+    return (coinsLeft - choice) % 4;
+  }
+
+  const handleTurn = e => {
+    const choice = Number(e.target.value);
     setLastPlayerMove(choice);
-    setCoinsLeft(coinsLeft - choice);
+
+    if (coinsLeft - choice === 0) {
+      setWinner('player');
+      setCoinsLeft(0);
+      return;
+    }
+
+    const nimChoice = impossibleNim(choice);
+    const coinsRemoved = choice + nimChoice;
+    setLastNimMove(nimChoice);
+
+    if (coinsLeft - coinsRemoved === 0) {
+      setWinner('nim');
+    }
+
+    setCoinsLeft(coinsLeft - coinsRemoved);
   }
 
   return (
     <div id="game-container">
       <p>How many coins?</p>
       <div id="choice-buttons">
-        <button className="choice" value={1} onClick={handleTurn}>1</button>
-        <button className="choice" value={2} onClick={handleTurn}>2</button>
-        <button className="choice" value={3} onClick={handleTurn}>3</button>
+        <button className="choice" value={1} onClick={handleTurn} disabled={coinsLeft < 1}>1</button>
+        <button className="choice" value={2} onClick={handleTurn} disabled={coinsLeft < 2}>2</button>
+        <button className="choice" value={3} onClick={handleTurn} disabled={coinsLeft < 3}>3</button>
       </div>
       <div id="coins-container">{coins}</div>
       <div id="game-messages">
         {lastPlayerMove && <p>You removed {lastPlayerMove} coins.</p>}
         {lastNimMove && <p>Nim removed {lastNimMove} coins.</p>}
+        {winner && <div id="game-over-msg">
+          <p>{winner === 'player' ? 'You win! Play again?' : 'Nim wins! Better luck next time.'}</p>
+          <button id="replay-button">Play Again</button>
+        </div>}
       </div>
     </div>
   )
